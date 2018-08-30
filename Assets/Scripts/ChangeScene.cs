b7a68b5b2528f38d97344e36a6b10c9e9e74e8a6
Rivindu Wanniachchi <RivinduW@99x.lk>
2018-08-30 16:37:40 +0530

    final commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour {
+
+	public void greetingCard()
+    {
+        SceneManager.LoadScene("greetingCardScene");
+    }
+
+
+    public void greetingMessage()
+    {
+        SceneManager.LoadScene("greetingMessageScene");
+    }
+
+    public void greetingCube()
+    {
+        SceneManager.LoadScene("greetingCubeScene");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit button clicked");
+    }
+}
